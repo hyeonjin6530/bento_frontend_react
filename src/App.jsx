5721e65b54454dc1f14c68c81f880d4bdf24fc9b
hyeonjin6530 <cola1876@naver.com>
2025-08-26 11:19:09 +0900

    feat: cohort-definition 페이지 라우팅 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-// App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import PersonLayout from './components/PersonLayout';
 
-import HomePage from './pages/HomePage';
-import CohortListPage from './pages/CohortListPage';
-import PersonSearchPage from './pages/PersonSearchPage';
-import PersonDetailPage from './pages/PersonDetailPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage'
+import HomePage from './pages/HomePage/HomePage.jsx';
+import CohortListPage from './pages/CohortListPage/CohortListPage.jsx';
+import PersonSearchPage from './pages/PersonSearchPage/PersonSearchPage.jsx';
+import PersonDetailPage from './pages/PersonDetailPage/PersonDetailPage.jsx';
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage/RegisterPage.jsx'
+import CohortDefinitionPage from "./pages/CohortDefinitionPage/CohortDefinitionPage.jsx";
+
 function App() {
   return (
     <Routes>
@@ -19,6 +20,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} /> 
         <Route index element={<HomePage />} />
         <Route path="cohort" element={<CohortListPage />} />
+        <Route path="/cohort-definition" element={<CohortDefinitionPage />} />
 
         <Route path="person" element={<PersonLayout />}>
           <Route index element={<PersonSearchPage />} />
